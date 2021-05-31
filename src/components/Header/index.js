25acf65b0ba39152e37443910dfa0dc637f0b944
Carlos Valencia <rasfreed@gmail.com>
2021-05-31 00:10:15 -0500

    First commit, proyect creation and Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+function Header() {
+
+
+  return(
+    <>
+      <header class="nav center">
+        <p class="logo">LOGO</p>
+        <input type="checkbox" name="" class="checkBtn"/>
+        <ol>
+          <a href="#"><li>Sobre mi</li></a>
+          <a href="#"><li>Educación</li></a>
+          <a href="#"><li>Experiencia</li></a>
+          <a href="#"><li>Proyectos</li></a>
+          <a href="#"><li>Contact</li></a>
+        </ol>
+      </header>
+    </>
+  )
+}
+
+export default Header
